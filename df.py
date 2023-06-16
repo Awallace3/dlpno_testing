3d6b479d9b23808cf24ad4db44d50cfd10f683ed
Austin Wallace <austinwallace196@gmail.com>
2023-06-16 13:29:07 -0400

    $@

--- df.py
+++ df.py
@@ -4,6 +4,14 @@ import matplotlib.pyplot as plt
 import qcelemental as qcel
 
 
+def sum_energies(e):
+    if type(e) == float:
+        return e
+    else:
+        return e[0]
+
+
+
 def main():
     """
     Read in s22 df for analysis
@@ -17,29 +25,29 @@ def main():
         "dlpno_ccsd_adz_orca_tightPNO",
         "dlpno_ccsd_adz_orca_veryTightPNO",
     ]
+    pd.set_option("display.max_columns", None)
     conv = qcel.constants.conversion_factor("hartree", "kcal/mol")
     df[energy_cols] = df[energy_cols] * conv
-    print(df[energy_cols])
     # Energy cell = [IE Energy, Dimer Energy, Monomer 1 Energy, Monomer 2 Energy]
     energy_cols_ie = [f"{i}_IE" for i in energy_cols]
     for n, i in enumerate(energy_cols):
         df[energy_cols_ie[n]] = df[i].apply(lambda x: x[0])
     print(df[['sys_ind', *energy_cols_ie]])
 
+    print("CCSD CP data")
     energy_cols = [
         # "dlpno_ccsd_adz",
         "dlpno_ccsd_adz_orca_loosePNO_CP",
         "dlpno_ccsd_adz_orca_normalPNO_CP",
         "dlpno_ccsd_adz_orca_tightPNO_CP",
-        # "dlpno_ccsd_adz_orca_veryTightPNO_CP",
+        "dlpno_ccsd_adz_orca_veryTightPNO_CP",
     ]
     conv = qcel.constants.conversion_factor("hartree", "kcal/mol")
     df[energy_cols] = df[energy_cols] * conv
-    print(df[energy_cols])
     # Energy cell = [IE Energy, Dimer Energy, Monomer 1 Energy, Monomer 2 Energy]
     energy_cols_ie = [f"{i}_IE" for i in energy_cols]
     for n, i in enumerate(energy_cols):
-        df[energy_cols_ie[n]] = df[i].apply(lambda x: x[0])
+        df[energy_cols_ie[n]] = df[i].apply(lambda x: sum_energies(x))
     print(df[['sys_ind', *energy_cols_ie]])
 
     print("CCSD(T) data")
@@ -48,31 +56,30 @@ def main():
         "dlpno_ccsd_t_adz_orca_loosePNO",
         "dlpno_ccsd_t_adz_orca_normalPNO",
         "dlpno_ccsd_t_adz_orca_tightPNO",
-        # "dlpno_ccsd_t_adz_orca_veryTightPNO",
+        "dlpno_ccsd_t_adz_orca_veryTightPNO",
     ]
     conv = qcel.constants.conversion_factor("hartree", "kcal/mol")
     df[energy_cols] = df[energy_cols] * conv
-    print(df[energy_cols])
     # Energy cell = [IE Energy, Dimer Energy, Monomer 1 Energy, Monomer 2 Energy]
     energy_cols_ie = [f"{i}_IE" for i in energy_cols]
     for n, i in enumerate(energy_cols):
-        df[energy_cols_ie[n]] = df[i].apply(lambda x: x[0])
+        df[energy_cols_ie[n]] = df[i].apply(lambda x: sum_energies(x))
     print(df[['sys_ind', *energy_cols_ie]])
 
+    print("CCSD(T) CP data")
     energy_cols = [
         # "dlpno_ccsd_adz",
         "dlpno_ccsd_t_adz_orca_loosePNO_CP",
         "dlpno_ccsd_t_adz_orca_normalPNO_CP",
         "dlpno_ccsd_t_adz_orca_tightPNO_CP",
-        # "dlpno_ccsd_t_adz_orca_veryTightPNO_CP",
+        "dlpno_ccsd_t_adz_orca_veryTightPNO_CP",
     ]
     conv = qcel.constants.conversion_factor("hartree", "kcal/mol")
     df[energy_cols] = df[energy_cols] * conv
-    print(df[energy_cols])
     # Energy cell = [IE Energy, Dimer Energy, Monomer 1 Energy, Monomer 2 Energy]
     energy_cols_ie = [f"{i}_IE" for i in energy_cols]
     for n, i in enumerate(energy_cols):
-        df[energy_cols_ie[n]] = df[i].apply(lambda x: x[0])
+        df[energy_cols_ie[n]] = df[i].apply(lambda x: sum_energies(x))
     print(df[['sys_ind', *energy_cols_ie]])
     return
 
